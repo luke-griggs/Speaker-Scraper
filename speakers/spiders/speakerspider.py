1ae3d3b709083d8cd35e34acff25570e7bee72d7
Luke Griggs <lukegriggs@Lukes-MacBook-Air.local>
2023-08-29 21:35:38 -0500

    used a for loop to scrap name and price of products from site

--- speakers/spiders/speakerspider.py
+++ speakers/spiders/speakerspider.py
@@ -7,4 +7,12 @@ class SpeakerspiderSpider(scrapy.Spider):
     start_urls = ["https://jbl.com/bluetooth-speakers/"]
 
     def parse(self, response):
+        products = response.css('div.tile-body')
+        for product in products:
+            item = {
+            'name' : product.css('a::text').get(),
+            'price' : product.css('span.value::text').get()
+            }
+            yield item
+
         pass
